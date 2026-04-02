f977a2e726b30a33a457bef9b809c33c2455c979
Eduardo Brandão <eduardob.fsa@gmail.com>
2026-04-02 13:27:34 -0300

    fix: remove unused getWorkflowPosts import from WorkflowDrawer

--- src/pages/entregas/components/WorkflowDrawer.tsx
+++ src/pages/entregas/components/WorkflowDrawer.tsx
@@ -10,7 +10,7 @@ import {
 import { SortableContext, useSortable, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import {
-  getWorkflowPosts, getWorkflowPostsWithProperties,
+  getWorkflowPostsWithProperties,
   addWorkflowPost, updateWorkflowPost, removeWorkflowPost,
   reorderWorkflowPosts, sendPostsToCliente, getPostApprovals, replyToPostApproval,
   completeEtapa,
